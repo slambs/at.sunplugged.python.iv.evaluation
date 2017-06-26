f9ac39432715301a654af362c484a92d892834fe
Sunplugged <Sunplugged@10.0.0.10>
2017-06-26 13:44:25 +0200

    Created Base Structure

---
+++ at.sunplugged.iv.evaluation/Evaluation/__init__.py
@@ -0,0 +1,26 @@
+
+def evaluateRawData( data ):
+    '''
+       Description...
+       
+       Parameter data: 2 Columns U and I data. 
+    '''
+    pass
+
+
+def evaluateRawDataLight( data ):
+    '''
+       Description...
+       
+       Parameter data: 2 Columns U and I data. 
+    '''
+    pass
+
+
+def evaluateRawDataDark( data ):
+    '''
+       Description...
+       
+       Parameter data: 2 Columns U and I data. 
+    '''
+    pass
